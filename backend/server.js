3afb6a3f8e273af657455d21963fbb7f0e44977c
ashusoni24 <24ashusoni@gmail.com>
2025-07-06 02:04:37 +0530

    added server upload logic

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import { configDotenv } from "dotenv";
 import { connect } from 'mongoose';
 import { connectDB } from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
+
+
+import path from 'path'
+import { fileURLToPath } from 'url';
+import resumeRoutes from './routes/resumeRoutes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 4000;
@@ -16,9 +27,25 @@ connectDB();
 
 app.use(express.json());
 
+
+app.use('/api/auth' , userRoutes)
+app.use('/api/resume', resumeRoutes);
+
+//server uploads
+
+app.use(
+  '/uploads',
+  express.static(path.join(__dirname, 'uploads'), {
+    setHeaders: (res, _path) => {
+      res.set('Access-Control-Allow-Origin','http://localhost:5173/' );
+    },
+  })
+);
+
+//routes
 app.get("/" , (req , res)=>{
     res.send('API IS WORKING')
 })
 app.listen(PORT , ()=>{
     console.log(`Server Started on http://localhost:${PORT}`)
-})
+}) 
