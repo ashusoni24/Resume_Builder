c2fd617c82f20bb93028971894ac2f8a1171cae6
ashusoni24 <24ashusoni@gmail.com>
2025-07-10 23:39:07 +0530

    route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+
 
 const App = () => {
   return (
-    <div>
-       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <Routes>
+        <Route path='/' element = {<LandingPage/>}/>
+    </Routes>
   )
 }
 
