25276036681340401fcfe741e15ce25d35264c13
ashusoni24 <24ashusoni@gmail.com>
2025-10-01 17:39:31 +0530

    db file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 export const connectDB = async ()=>{
-    await mongoose.connect('Your db link')
+    await mongoose.connect('mongodb+srv://ashu:NtX2uLzrNCmz8ZB6@cluster0.y1lim1n.mongodb.net/RESUME')
     .then(()=>{
         console.log("DB connected")
     })
